f933723d051a284a3ea23abdf708baad1ae06aa4
DenLopes <denis.gnl@gmail.com>
2023-05-29 19:10:44 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Example from "./components/Example.vue";
+</script>
+
+<template>
+  <div class="flex flex-col justify-center bg-purple-600 h-screen">
+    <div class="flex justify-center">
+      <div class="flex flex-col">
+      <span class="text-black font-semibold text-lg mb-2">Digite para concatenar.</span>
+      <Example />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
