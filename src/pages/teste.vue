7f18b777ecfb77217fbeaeb2ba34cf842879a220
Denis <denis.gnl@gmail.com>
2023-06-06 13:09:56 -0300

    começando a configurar o grafico

--- src/pages/teste.vue
+++ src/pages/teste.vue
@@ -1,25 +1,39 @@
 <script setup>
-import { SourceMapConsumer } from 'source-map-js/lib/source-map-consumer';
-import { ref, onMounted, pushScopeId } from 'vue'
-import dadosIbama from '../assets/Ibama.json'
+import Chart from "chart.js/auto";
+import { ref, onMounted } from "vue";
+import dadosIbama from "../assets/Ibama.json";
 
 const dados = dadosIbama;
 const tipoReceita = ref([]);
+const canvas = ref(null);
 
 const separaReceita = () => {
-	tipoReceita.value = dados.data.map(inf =>{
-		if(tipoReceita.value.find(inf => tipoReceita.value == inf.receita)){
-			return inf;
-		}
-	});
-}
-
+  dados.data.forEach((item) => {
+    if (!tipoReceita.value.includes(item.receita)) {
+      tipoReceita.value.push(item.receita);
+    }
+  });
+};
 
 onMounted(() => {
-	console.log(dados);
-	separaReceita();
-	console.log(tipoReceita.value);
-})
+  canvas.value = document.querySelector("canvas");
+
+  const chartConfig = {
+    title: "My Chart",
+    xAxis: ["Label 1", "Label 2", "Label 3"],
+    yAxis: [10, 20, 30],
+  };
+
+  new Chart(canvas, chartConfig);
+	
+  console.log(dados);
+  separaReceita();
+  console.log(tipoReceita.value);
+});
 </script>
 
-<template></template>
+<template>
+  <div>
+    <canvas ref="canvas"></canvas>
+  </div>
+</template>
