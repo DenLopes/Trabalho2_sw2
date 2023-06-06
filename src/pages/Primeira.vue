7a613ce520a6001585eabb2134b33d5094c046b5
DenLopes <denis.gnl@gmail.com>
2023-06-05 22:50:09 -0300

    Mais umas coisas que eu fiz ai

--- src/pages/Primeira.vue
+++ src/pages/Primeira.vue
@@ -1,23 +1,31 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
-  <div class="flex flex-col bg-green-600 w-full h-full">
-    <div class="flex w-full">
+  <div class="flex flex-col justify-between bg-green-600 w-full min-h-full">
+    <div class="text-center mt-36 w-full">
       <span
-        class="w-fit mx-auto mt-20 mb-10 font-bold text-[11rem] text-transparent bg-clip-text bg-gradient-to-b from-lime-300 to-yellow-300"
+        class="text-center font-bold text-[11rem] text-transparent bg-clip-text bg-gradient-to-b from-lime-300 to-yellow-300 hover:animate-spin"
         >IBAMA</span
       >
-    </div>
-    <div class="flex h-full">
-      <span class="font-bold text-5xl mx-auto text-lime-300">
-        Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais Renovaveis
-      </span>
+      <div class="text-center flex h-full mb-12">
+        <span class="font-bold text-5xl mx-auto text-lime-300">
+          Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais
+          Renovaveis
+        </span>
+      </div>
     </div>
     <div class="flex justify-center mb-20">
-      <router-link to="/inf" class="btn w-40 h-16 text-white border border-solid border-white text-lg">Descubra</router-link>
+      <router-link
+        to="/inf"
+        class="btn w-40 h-16 text-white border border-solid border-white text-lg"
+        >Descubra</router-link
+      >
     </div>
-    <div class="flex p-4">  
+    <div class="flex justify-between p-4">
       <img src="../assets/ibama-logo-1.png" class="mt-auto w-32 h-32" />
+      <img src="../assets/efcf9caf-6d29-4c24-8266-0b7366ea3a40.png" class="mt-auto w-72" />
     </div>
   </div>
 </template>
--- src/pages/teste.vue
+++ src/pages/teste.vue
@@ -1,9 +1,25 @@
 <script setup>
-    import graphic from '../components/graphic.vue';
+import { SourceMapConsumer } from 'source-map-js/lib/source-map-consumer';
+import { ref, onMounted, pushScopeId } from 'vue'
+import dadosIbama from '../assets/Ibama.json'
+
+const dados = dadosIbama;
+const tipoReceita = ref([]);
+
+const separaReceita = () => {
+	tipoReceita.value = dados.data.map(inf =>{
+		if(tipoReceita.value.find(inf => tipoReceita.value == inf.receita)){
+			return inf;
+		}
+	});
+}
+
+
+onMounted(() => {
+	console.log(dados);
+	separaReceita();
+	console.log(tipoReceita.value);
+})
 </script>
 
-<template>
-
-   <graphic/>    
-
-</template>
+<template></template>
