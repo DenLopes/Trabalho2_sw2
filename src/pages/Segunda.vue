f4266ea71a5df907559d1bef1a6f2ceaeadb6b04
Talvezcarvalho <gabrielassis170@gmail.com>
2023-06-06 21:51:35 -0300

    Atualizei umas caralhas

--- src/pages/Segunda.vue
+++ src/pages/Segunda.vue
@@ -3,10 +3,26 @@
 </script>
 
 <template>
-	<div class="flex justify-center bg-emerald-300 h-full">
+	<div class="bg-slate-900 h-full">
 		<div class="flex w-[85%]">
-			<span class=" font-extrabold text-lg">ASDASDASDASDASDADASDASDASDASDASDDASDASDASDDASD</span>
 			<RouterLink to="/" class="btn btn-ghost normal-case text-xl">Vai para primeira</RouterLink>
+		</div>
+
+		<div class="flex flex-col justify-between content-center p-8 text-white text-xl">
+			<h2 class="text-green-500 text-center text-2xl font-bold mb-28">
+				O que é o IBAMA?
+			</h2>
+			<div class="flex flex-row m-4 justify-around mb-40">
+				<p class="w-[70%] leading-9">A Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusantium <strong class="text-green-500">repellendus</strong> dolore. Quas modi facere voluptate blanditiis consequuntur, dicta perspiciatis expedita laboriosam obcaecati dolores. Dolor at explicabo sunt ut maxime? a Lorem ipsum dolor sit amet consectetur adipisicing elit. <strong class="text-green-500">Adipisci</strong> beatae quod minima sapiente porro! Accusantium, explicabo! Adipisci omnis, at dolor voluptate quaerat tempore ex labore est explicabo veniam fugit ipsam?
+				</p>
+				<img src="../assets/brasil.png" alt="brasil" class="w-44 h-44">
+			</div>
+			<div class="flex flex-row m-4 justify-around">
+				<span class="text-green-500">GRAFICO</span>
+				<p class="w-[70%]">A Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusantium repellendus dolore. Quas modi facere voluptate blanditiis consequuntur, dicta perspiciatis expedita laboriosam obcaecati dolores. Dolor at explicabo sunt ut maxime? a Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci beatae quod minima sapiente porro! Accusantium, explicabo! Adipisci omnis, at dolor voluptate quaerat tempore ex labore est explicabo veniam fugit ipsam?
+				</p>
+			</div>
+
 		</div>
 	</div>
 </template>
