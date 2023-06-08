4398b537db14df43bde2b9721ce5b662d70938c6
Talvezcarvalho <gabrielassis170@gmail.com>
2023-06-08 19:15:13 -0300

    alguma coisa qque voce fez

--- src/pages/Segunda.vue
+++ src/pages/Segunda.vue
@@ -1,99 +1,97 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
-  <div class="bg-slate-900 h-full">
+  <div class="bg-slate-900 min-h-screen">
+
     <div class="flex w-[85%]">
       <RouterLink to="/" class="btn btn-ghost normal-case text-xl"
         >Vai para primeira</RouterLink
       >
     </div>
 
-    <div
-      class="flex flex-col justify-between content-center p-8 text-white text-xl"
-    >
-      <h2 class="text-green-500 text-center text-2xl font-bold mb-28">
-        O que é o IBAMA?
-      </h2>
-      <div class="flex flex-row m-4 justify-around mb-40">
-        <p class="w-[70%] leading-9">
-          O Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais
-          Renováveis (IBAMA) foi criado em 1989, por meio da Lei nº 7.735, com o
-          objetivo de unificar e fortalecer as ações de proteção e conservação
-          do meio ambiente no Brasil. Desde sua criação, o IBAMA tem
-          desempenhado um papel essencial na proteção ambiental do país. O órgão
-          atua em âmbito nacional, sendo vinculado ao Ministério do Meio
-          Ambiente, e possui uma estrutura descentralizada, com escritórios
-          regionais em todo o território brasileiro. Ao longo de sua história, o
-          IBAMA tem enfrentado diversos desafios e desenvolvido ações para
-          combater crimes ambientais, promover a conservação da biodiversidade e
-          assegurar o uso sustentável dos recursos naturais. Alguns marcos
-          importantes na história do IBAMA incluem:
-        </p>
-        <img src="../assets/brasil.png" alt="brasil" class="w-44 h-44" />
-      </div>
-      <div class="flex flex-row m-4 justify-around">
-        <span class="text-green-500">GRAFICO</span>
-        <p class="w-[70%]">
-          Combate ao desmatamento: O IBAMA tem desempenhado um papel fundamental
-          no combate ao desmatamento ilegal na Amazônia e em outras regiões do
-          país. Por meio de operações de fiscalização e monitoramento, o órgão
-          busca identificar áreas desmatadas irregularmente, autuar os
-          responsáveis e promover a recuperação de áreas degradadas.
-        </p>
-      </div>
-      <div class="flex flex-row m-4 justify-around mb-40">
-        <p class="w-[70%] leading-9">
-          Proteção da fauna e flora: O IBAMA trabalha para combater o tráfico de
-          animais silvestres, promover a conservação de espécies ameaçadas de
-          extinção e proteger a flora brasileira. O órgão emite licenças para
-          atividades que envolvem animais e plantas, realiza resgates e
-          reintroduções de espécies em perigo, e desenvolve programas de
-          conservação.
-        </p>
-        <img src="" alt="brasil" class="w-44 h-44" />
-      </div>
-      <div class="flex flex-row m-4 justify-around">
-        <img src="" alt="brasil" class="w-44 h-44" />
-        <p class="w-[70%]">
-          Criação e gestão de unidades de conservação: O IBAMA tem participado
-          ativamente na criação e gestão de unidades de conservação no Brasil,
-          como parques nacionais, reservas biológicas e áreas de proteção
-          ambiental. Essas áreas protegidas desempenham um papel fundamental na
-          preservação de ecossistemas, na conservação da biodiversidade e no
-          desenvolvimento de pesquisas científicas.
-        </p>
-      </div>
-      <div class="flex flex-row m-4 justify-around mb-40">
-        <p class="w-[70%] leading-9">
-          Fiscalização e aplicação de sanções: O IBAMA possui uma importante
-          atuação na fiscalização e aplicação de sanções administrativas para
-          infrações e crimes ambientais. Os agentes do IBAMA realizam operações
-          de campo, autuam os responsáveis por irregularidades e aplicam multas
-          e outras penalidades previstas em lei.
-        </p>
-        <img src="" alt="brasil" class="w-44 h-44" />
-      </div>
-      <div class="flex flex-row m-4 justify-around">
-        <img src="" alt="brasil" class="w-44 h-44" />
-        <p class="w-[70%]">
-          Educação ambiental: O IBAMA promove a educação e conscientização
-          ambiental por meio de campanhas, programas e projetos que visam
-          sensibilizar a população sobre a importância da conservação do meio
-          ambiente. São desenvolvidas ações educativas em escolas, comunidades e
-          meios de comunicação, buscando incentivar práticas sustentáveis e a
-          participação da sociedade na proteção ambiental.
-        </p>
-      </div>
-      <div class="flex flex-center m-4">
-        <p class="w-[70%]">
-          É importante destacar que o IBAMA atua em parceria com outros órgãos
-          governamentais, como as polícias federal e militar ambiental, e com
-          entidades da sociedade civil para fortalecer a proteção ambiental no
-          Brasil.
-        </p>
-      </div>
-    </div>
-  </div>
+		<div class="flex flex-col justify-between content-center p-8 text-white text-xl">
+			<h2 class="text-green-500 text-center text-3xl font-bold mb-28">
+				O QUE É O IBAMA?
+			</h2>
+			<div class="flex flex-row m-4 justify-around mb-40 items-center">
+				<p class="w-[70%] leading-10 text-justify text-2xl">O Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais Renováveis <strong class="text-green-500">(IBAMA)</strong>
+					 foi criado em 1989, por meio da Lei nº 7.735, 
+					com o objetivo de unificar e fortalecer as ações de proteção
+					 e conservação do meio ambiente no Brasil.
+					 Desde sua criação, o IBAMA tem desempenhado um papel essencial na <strong class="text-green-500">proteção ambiental do país.</strong> 
+					 O órgão atua em âmbito nacional, sendo vinculado ao Ministério do Meio Ambiente,
+					 e possui uma estrutura descentralizada, com escritórios regionais em todo o território brasileiro.
+					 Ao longo de sua história, o IBAMA tem enfrentado diversos <strong class="text-green-500">desafios</strong> e desenvolvido ações para combater 
+					 crimes ambientais, promover a conservação da biodiversidade e 
+					 assegurar o uso sustentável dos recursos naturais.
+				</p>
+				<img src="../assets/brasil.png" alt="brasil" class="w-44 h-44">
+			</div>
+			<div class="flex flex-row m-4 justify-around items-center">
+				<div class="flex justify-center items-center">
+					<span class="text-green-500 font-bold text-5xl text-center">1.87<br>BILHÃO</span>
+					<span class="text-green-500 font-bold text-8xl ml-2">R$</span>
+				</div>
+				<p class="w-[70%] text-2xl leading-10"> É o orçamento previsto para o ano de 2023, mas para onde vai todo esté gasto?(insirir um texto decente.)
+				</p>
+			</div>
+			<button class="btn w-40 h-12 bg-green-500 self-center mt-5 text-white">
+				Veja mais
+				</button>	
+			<div class="flex flex-col items-center mt-32">
+				<h2 class="text-yellow-300 text-3xl font-bold">ATUAÇÕES DO IBAMA</h2>
+				<p class="mt-16 text-2xl w-[90%] leading-10">
+					Entenda sobre as atuações do IBAMA. a Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quisquam fugit, accusantium commodi praesentium quo esse quidem, sint quis ea nemo ratione temporibus est. Atque quisquam vero nisi quam distinctio!(INSIRA MAIS TEXTO, usar STRONG para <strong class="text-yellow-300">negrito</strong> com text-yellow-300).
+				</p>
+			</div>
+			<div class="flex justify-between"></div>
+
+
+
+
+
+
+
+
+
+
+			<div class="flex flex-row m-4 justify-around mb-40 mt-96">
+				<p class="w-[70%] leading-9">Proteção da fauna e flora: O IBAMA trabalha para combater o tráfico de animais silvestres, promover a conservação de 
+					espécies ameaçadas de extinção e proteger a flora brasileira. O órgão emite licenças para atividades que envolvem animais e plantas, 
+					realiza resgates e reintroduções de espécies em perigo, e desenvolve programas de conservação.
+				</p>
+				<img src="" alt="brasil" class="w-44 h-44">
+			</div>
+			<div class="flex flex-row m-4 justify-around">
+				<img src="" alt="brasil" class="w-44 h-44">
+				<p class="w-[70%]">Criação e gestão de unidades de conservação: O IBAMA tem participado ativamente na criação e gestão de unidades 
+					de conservação no Brasil, como parques nacionais, reservas biológicas e áreas de proteção ambiental. Essas áreas protegidas desempenham um papel 
+					fundamental na preservação de ecossistemas, na conservação da biodiversidade e no desenvolvimento de pesquisas científicas.
+				</p>
+			</div>
+			<div class="flex flex-row m-4 justify-around mb-40">
+				<p class="w-[70%] leading-9">Fiscalização e aplicação de sanções: O IBAMA possui uma importante atuação na 
+					fiscalização e aplicação de sanções administrativas para infrações e crimes ambientais. Os agentes do IBAMA realizam operações de campo, 
+					autuam os responsáveis por irregularidades e aplicam multas e outras penalidades previstas em lei.
+				</p>
+				<img src="" alt="brasil" class="w-44 h-44">
+			</div>
+			<div class="flex flex-row m-4 justify-around">
+				<img src="" alt="brasil" class="w-44 h-44">
+				<p class="w-[70%]">Educação ambiental: O IBAMA promove a educação e conscientização ambiental por meio de campanhas, programas e projetos que 
+					visam sensibilizar a população sobre a importância da conservação do meio ambiente. São desenvolvidas ações educativas em escolas, 
+					comunidades e meios de comunicação, buscando incentivar práticas sustentáveis e a participação da sociedade na proteção ambiental.
+				</p>
+			</div>
+			<div class="flex items-center m-4">
+				<p class="w-[70%]">É importante destacar que o IBAMA atua em parceria com outros órgãos governamentais, como as polícias federal e militar ambiental, 
+					e com entidades da sociedade civil para fortalecer a proteção ambiental no Brasil.
+				</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped></style>
