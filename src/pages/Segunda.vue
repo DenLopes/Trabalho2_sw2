6c501e00b81e1c76195c4a14f46672046ca47b74
DenLopes <denis.gnl@gmail.com>
2023-06-11 14:15:16 -0300

    pequenas mudanças

--- src/pages/Segunda.vue
+++ src/pages/Segunda.vue
@@ -1,49 +1,61 @@
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, watch } from "vue";
 import Grafico from "../components/Grafico.vue";
 import dadosIbama from "../assets/Ibama.json";
 
 const dados = dadosIbama;
 const tiposReceita = ref([]);
-const receitaSelecionada = ref('Selecione o tipo de receita');
+const receitaSelecionada = ref("Selecione o tipo de receita");
 
+//refs para pegar elemantos do template e mudar o css
+const refCorpoText = ref(null);
+const refDivCorpoText = ref(null);
+
+//função que separa e seleciona as receitas
 const separaReceita = () => {
+  //pega um array com todos os tipos de receita
   dados.data.forEach((item) => {
     if (!tiposReceita.value.includes(item.receita)) {
       tiposReceita.value.push(item.receita);
     }
   });
-  //tira dos tipo onde tme pouco dados
-	let newTiposReceita = [];
-tiposReceita.value.forEach((item) => {
-  let selecionada = dados.data.filter(i => i.receita == item);
-  if(selecionada.length >= 8){
-    newTiposReceita.push(item);
-  }
-});
-
-tiposReceita.value = newTiposReceita;
-
+  //coloca só os tipos de receita que tem mais de 7 anos
+  let newTiposReceita = [];
+  tiposReceita.value.forEach((item) => {
+    let selecionada = dados.data.filter((i) => i.receita == item);
+    if (selecionada.length >= 8) {
+      newTiposReceita.push(item);
+    }
+  });
+  //coloca os tipos de receita no ref
+  tiposReceita.value = newTiposReceita;
 };
 
+//computed para pegar os anos de cada receita selecionada
 const xAxis = computed(() => {
-	if(receitaSelecionada.value != 'Selecione o tipo de receita'){
-		return dados.data.filter(item => item.receita == receitaSelecionada.value).map(item => item.ano);
-	}
-	return [];
-});
-
-const yAxis = computed(() => {
-	if(receitaSelecionada.value!= 'Selecione o tipo de receita'){
-    return dados.data.filter(item => item.receita == receitaSelecionada.value).map(item => item.valor);
+  if (receitaSelecionada.value != "Selecione o tipo de receita") {
+    return dados.data
+      .filter((item) => item.receita == receitaSelecionada.value)
+      .map((item) => item.ano);
   }
   return [];
 });
 
+//computed para pegar os valores de cada receita selecionada
+const yAxis = computed(() => {
+  if (receitaSelecionada.value != "Selecione o tipo de receita") {
+    return dados.data
+      .filter((item) => item.receita == receitaSelecionada.value)
+      .map((item) => item.valor);
+  }
+  return [];
+});
+
+//array de botoes para o texto do corpo
 const botoes = ref([
   {
     nome: "Proteção animal",
-    estado: true,
+    estado: false,
     classe: "bg-yellow-400 text-black hover:text-white",
   },
   {
@@ -58,26 +70,30 @@ const botoes = ref([
   },
 ]);
 
+//array de texto do corpo
 const textoBotoes = ref([
   {
-    texto: "Ecrava algo sobre Proteção animal, TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO",
+    texto:
+      "Ecrava algo sobre Proteção animal, TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO",
     estado: true,
-    classe: "bg-yellow-500",
+    classe: 'bg-yellow-500',
   },
   {
-    texto: "Ecrava algo sobre Proteção Da Flora, TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO",
+    texto:
+      "Ecrava algo sobre Proteção Da Flora, TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO",
     estado: false,
-    classe: "bg-pink-500",
+    classe: 'bg-pink-500',
   },
   {
-    texto: "Ecrava algo sobre Atuação Politica, TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO",
+    texto:
+      "Ecrava algo sobre Atuação Politica, TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOO TEXTOOOOOOOOOOOTEXTOOOOOOOOOOOTEXTOOOOOOOOOOO",
     estado: false,
-    classe: "bg-lime-500",
+    classe: 'bg-lime-500',
   },
 ]);
 
-const mudaEstadoBotao = (val, index) => {
-	console.log(val[index].estado);
+//função para mudar o estado do botão
+const mudaEstado = (val, index) => {
   for (var i = 0; i < val.length; i++) {
     if (i == index) {
       val[i].estado = true;
@@ -87,16 +103,34 @@ const mudaEstadoBotao = (val, index) => {
   }
 };
 
+//gambiarra para o testo ter o height to tamanho do texto mesmo sendo absolute
+watch(botoes.value, () => {
+  if(refCorpoText.value != null){
+    refCorpoText.value.forEach((item, index) => {
+      if (botoes.value[index].estado) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+      refDivCorpoText.value[index].style.height = refCorpoText.value[index].clientHeight + 'px';
+    })  
+  }
+})
+
 onMounted(() => {
   separaReceita();
+  //gambiarra para o dar update no texto
+  mudaEstado(botoes.value, 0);
 });
 </script>
 
 <template>
   <div class="relative bg-gray-950 min-h-screen">
-      <RouterLink to="/" class="sticky top-4 mt-4 ml-4 btn text-center items-center font-bold rounded-full bg-gray-100 text-black normal-case text-xl hover:bg-white z-50"
-        >{{ "<" }}</RouterLink
-      >
+    <RouterLink
+      to="/"
+      class="sticky top-4 mt-4 ml-4 btn text-center items-center font-bold rounded-full bg-gray-100 text-black normal-case text-xl hover:bg-white z-50"
+      >{{ "<" }}</RouterLink
+    >
 
     <div
       class="flex flex-col justify-between content-center p-8 text-white text-xl"
@@ -123,28 +157,38 @@ onMounted(() => {
         </p>
         <img src="../assets/brasil.png" alt="brasil" class="w-44 h-44" />
       </div>
-      <div class="flex flex-row m-4 justify-around items-center">
-        <div class="flex justify-center items-center">
+      <div class="flex m-4 justify-around">
+        <div class="flex">
           <span class="text-green-500 font-bold text-5xl text-center"
             >1.87<br />BILHÃO</span
           >
           <span class="text-green-500 font-bold text-8xl ml-2">R$</span>
         </div>
-        <p class="w-[70%] text-2xl leading-10">
-          É o orçamento previsto para o ano de 2023, mas para onde vai todo esté
-          gasto?(insirir um texto decente.)
-        </p>
-      </div>
-			<div class="flex justify-end w-full h-full">
-		<div class="flex justify-center">
-			<select v-model="receitaSelecionada" class="select select-bordered w-full max-w-xs mr-2 text-black">
-				<option disabled selected>Selecione o tipo de receita</option>
-				<option v-for="(tipoReceita, index) in tiposReceita" :key="index">{{ tipoReceita }}</option>
-			</select>
-			<Grafico :xAxis="xAxis" :yAxis="yAxis" :label="receitaSelecionada" :key="receitaSelecionada"/>
-		</div>
-  </div>
 
+        <div class="flex flex-col justify-center">
+          <p class="w-[70%] text-2xl leading-10 mb-auto">
+            É o orçamento previsto para o ano de 2023, mas para onde vai todo
+            esté gasto?(insirir um texto decente.)
+          </p>
+          <div class="flex mt-10">
+            <select
+              v-model="receitaSelecionada"
+              class="select select-bordered w-full max-w-xs mr-2 text-black"
+            >
+              <option disabled selected>Selecione o tipo de receita</option>
+              <option v-for="(tipoReceita, index) in tiposReceita" :key="index">
+                {{ tipoReceita }}
+              </option>
+            </select>
+            <Grafico
+              :xAxis="xAxis"
+              :yAxis="yAxis"
+              :label="receitaSelecionada"
+              :key="receitaSelecionada"
+            />
+          </div>
+        </div>
+      </div>
       <div class="flex flex-col items-center mt-24">
         <h2 class="text-yellow-300 text-3xl font-bold">ATUAÇÕES DO IBAMA</h2>
         <p class="mt-16 text-2xl w-[90%] leading-10">
@@ -157,13 +201,13 @@ onMounted(() => {
         </p>
       </div>
 
-      <div class="flex flex-col mt-12">
+      <div class="flex flex-col mt-12 mb-36">
         <div class="flex justify-around mb-12">
           <button
             v-for="(botao, index) in botoes"
             @click="
-              mudaEstadoBotao(botoes, index),
-              mudaEstadoBotao(textoBotoes, index)
+              mudaEstado(botoes, index),
+              mudaEstado(textoBotoes, index)
             "
             :class="botao.classe"
             class="btn"
@@ -171,24 +215,28 @@ onMounted(() => {
             {{ botao.nome }}
           </button>
         </div>
-        <div
-          v-for="(texto, index) in textoBotoes"
-          :key="index"
-        >
-				<transition
-        enter-active-class="duration-300 ease-out"
-        enter-from-class="transform opacity-0"
-        enter-to-class="opacity-100"
+        <div ref="refDivCorpoText" v-for="(texto, index) in textoBotoes" :key="index" class="flex">
+          <transition
+        enter-active-class="duration-100 ease-out"
+        enter-from-class="transform opacity-0 translate-x-full"
+        enter-to-class="opacity-50 translate-x-0"
         leave-active-class="duration-200 ease-in"
-        leave-from-class="opacity-100"
-        leave-to-class="transform opacity-0"
-      	>
-				<span class="absolute w-[calc(100vw-100px)]" v-if="texto.estado" :class="texto.classe">{{ texto.texto }}</span>
-				</transition>
-				</div>
+        leave-from-class="opacity-100 translate-x-0"
+        leave-to-class="transform opacity-0 translate-x-full"
+          >
+            <span
+            ref="refCorpoText"
+              class="absolute w-[calc(100vw-100px)]"
+              v-show="texto.estado"
+              :key="index"
+              :class="texto.classe"
+              >{{ texto.texto }}</span
+            >
+          </transition>
+        </div>
       </div>
 
-      <div class="flex flex-row m-4 justify-around mb-40 mt-96">
+      <div class="flex flex-row m-4 justify-around">
         <p class="w-[70%] leading-9">
           Proteção da fauna e flora: O IBAMA trabalha para combater o tráfico de
           animais silvestres, promover a conservação de espécies ameaçadas de
